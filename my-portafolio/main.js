2e44529da724b515530ce0eb3fc21abd1ac833d7
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-10-30 18:12:26 +0000

    corregidoErik

--- my-portafolio/main.js
+++ my-portafolio/main.js
@@ -3,7 +3,7 @@ import { changeTheme } from "./components/Navbar/Navbar";
 import { linkPage } from "./utils/linkPage";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer.js";
-import { Home } from "./pages/home/home.js";
+import  {home} from "./pages/home/home.js";
 import { Projects } from "./pages/Projects/Projects";
 import { Divider } from "./components/Divider/Divider";
 
@@ -12,10 +12,10 @@ header.innerHTML = Navbar();
 const footer = document.querySelector("footer");
 footer.innerHTML = Footer();
 
-linkPage("#homelink", Home);
+linkPage("#homelink", home);
 linkPage("#projectslink", Projects);
 
-Home();
+home();
 
 changeTheme();
 
--- my-portafolio/pages/home/home.js
+++ my-portafolio/pages/home/home.js
@@ -1,17 +1,16 @@
-import "./Home.css";
+import "./home.css";
 import { cleanPage } from "../../utils/cleanPage";
 
-const Home = () => {
+export const home = () => {
   const main = document.querySelector("main");
   cleanPage(main);
-  main.innerHTML = 
-    <section class="home">
+  main.innerHTML = `
+    <section class= "home">
     <p>Hey, I'm</p>
     <h1>Peter Parker</h1>
     <p>I'm a software engineer based in Toronto, Canada and also a communication and journalism student. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</p>
     <a href="mailto:peterparker@gmail.com">Say hi →</a>
-    </section>;
+    </section>`; 
 };
 
 
-export default Home;
